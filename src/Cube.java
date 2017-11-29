9c44d2a01e19b2c1766bac7668b2228f230fa122
Trent Gierhart <trentgierhart@gmail.com>
2017-11-29 10:33:17 -0600

    working on cube class

---
+++ src/Cube.java
@@ -0,0 +1,156 @@
+
+public class Cube {
+
+/*[side][row][column]
+ * [side]:
+ * * 0=top
+ * * 1=front
+ * * 2=left
+ * * 3=right
+ * * 4=back
+ * * 5=bottom
+ * 
+ * [row]:
+ * * 0=top
+ * * 1=middle
+ * * 2=bottom
+ * 
+ * [column]:
+ * * 0=left
+ * * 1=center
+ * * 2=right
+ */
+private char[][][] sides;
+
+//Returns a copy of the cube's current state
+private char[][][] copyState(char[][][] original){
+	char[][][] copy = new char[6][3][3];
+	for (int a = 0; a<6; a++){
+		for (int b=0; b<3; b++){
+			for (int c=0; c<3; c++){
+				copy[a][b][c] = original[a][b][c];
+			}
+		}
+	}
+	return copy;
+}
+
+//True rotates the top of the cube clockwise, false rotates it counterclockwise
+public void turnHorizontal(int horizontalIndex, boolean isClockWise){
+	char[][][] copy = copyState(sides);
+	//TOP CW
+	if (horizontalIndex==0 && isClockWise){
+		//change top
+		copy[0][0][0] = sides[0][2][0];
+		copy[0][0][1] = sides[0][1][0];
+		copy[0][0][2] = sides[0][0][0];
+		copy[0][1][0] = sides[0][2][1];
+		copy[0][1][2] = sides[0][0][1];
+		copy[0][2][0] = sides[0][2][2];
+		copy[0][2][1] = sides[0][1][2];
+		copy[0][2][2] = sides[0][0][2];
+		//change front
+		copy[1][0][0] = sides[3][0][0];
+		copy[1][0][1] = sides[3][0][1];
+		copy[1][0][2] = sides[3][0][2];
+		//change left
+		copy[2][0][0] = sides[1][0][0];
+		copy[2][0][1] = sides[1][0][1];
+		copy[2][0][2] = sides[1][0][2];
+		//change right
+		copy[3][0][0] = sides[4][0][0];
+		copy[3][0][1] = sides[4][0][1];
+		copy[3][0][2] = sides[4][0][2];
+		//change back
+		copy[4][0][0] = sides[2][0][0];
+		copy[4][0][1] = sides[2][0][1];
+		copy[4][0][2] = sides[2][0][2];	
+	}
+	//TOP CCW
+	else if (horizontalIndex==0){
+		//change top
+		copy[0][0][0] = sides[0][0][2];
+		copy[0][0][1] = sides[0][1][2];
+		copy[0][0][2] = sides[0][2][2];
+		copy[0][1][0] = sides[0][0][1];
+		copy[0][1][2] = sides[0][2][1];
+		copy[0][2][0] = sides[0][0][0];
+		copy[0][2][1] = sides[0][1][0];
+		copy[0][2][2] = sides[0][2][0];
+		//change front
+		copy[1][0][0] = sides[2][0][0];
+		copy[1][0][1] = sides[2][0][1];
+		copy[1][0][2] = sides[2][0][2];
+		//change left
+		copy[2][0][0] = sides[4][0][0];
+		copy[2][0][1] = sides[4][0][1];
+		copy[2][0][2] = sides[4][0][2];
+		//change right
+		copy[3][0][0] = sides[1][0][0];
+		copy[3][0][1] = sides[1][0][1];
+		copy[3][0][2] = sides[1][0][2];
+		//change back
+		copy[4][0][0] = sides[3][0][0];
+		copy[4][0][1] = sides[3][0][1];
+		copy[4][0][2] = sides[3][0][2];
+	}
+	//MIDDLE CW
+	if (horizontalIndex==1 && isClockWise){
+		//change top
+		copy[0][0][0] = sides[0][2][0];
+		copy[0][0][1] = sides[0][1][0];
+		copy[0][0][2] = sides[0][0][0];
+		copy[0][1][0] = sides[0][2][1];
+		copy[0][1][2] = sides[0][0][1];
+		copy[0][2][0] = sides[0][2][2];
+		copy[0][2][1] = sides[0][1][2];
+		copy[0][2][2] = sides[0][0][2];
+		//change front
+		copy[1][0][0] = sides[3][0][0];
+		copy[1][0][1] = sides[3][0][1];
+		copy[1][0][2] = sides[3][0][2];
+		//change left
+		copy[2][0][0] = sides[1][0][0];
+		copy[2][0][1] = sides[1][0][1];
+		copy[2][0][2] = sides[1][0][2];
+		//change right
+		copy[3][0][0] = sides[4][0][0];
+		copy[3][0][1] = sides[4][0][1];
+		copy[3][0][2] = sides[4][0][2];
+		//change back
+		copy[4][0][0] = sides[2][0][0];
+		copy[4][0][1] = sides[2][0][1];
+		copy[4][0][2] = sides[2][0][2];	
+	}
+	//MIDDLE CCW
+	else if (horizontalIndex==1){
+		//change top
+		copy[0][0][0] = sides[0][0][2];
+		copy[0][0][1] = sides[0][1][2];
+		copy[0][0][2] = sides[0][2][2];
+		copy[0][1][0] = sides[0][0][1];
+		copy[0][1][2] = sides[0][2][1];
+		copy[0][2][0] = sides[0][0][0];
+		copy[0][2][1] = sides[0][1][0];
+		copy[0][2][2] = sides[0][2][0];
+		//change front
+		copy[1][0][0] = sides[2][0][0];
+		copy[1][0][1] = sides[2][0][1];
+		copy[1][0][2] = sides[2][0][2];
+		//change left
+		copy[2][0][0] = sides[4][0][0];
+		copy[2][0][1] = sides[4][0][1];
+		copy[2][0][2] = sides[4][0][2];
+		//change right
+		copy[3][0][0] = sides[1][0][0];
+		copy[3][0][1] = sides[1][0][1];
+		copy[3][0][2] = sides[1][0][2];
+		//change back
+		copy[4][0][0] = sides[3][0][0];
+		copy[4][0][1] = sides[3][0][1];
+		copy[4][0][2] = sides[3][0][2];
+	}
+	sides = copyState(copy);
+}//end rotateTop
+
+}//end class
