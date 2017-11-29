7cc03151ebfc8ff37987f2280630874f232706a3
Justine Onnen <justinefawno@gmail.com>
2017-11-29 10:20:40 -0600

    made cube class

---
+++ Cube.java
@@ -0,0 +1,17 @@
+
+public class Cube {
+	private char[][][] sides;
+	private ArrayList <SolveInstruction> Instructions = new ArrayList <> ();
+	private int currentStep;
+	
+	public void scramble()
+	{
+		
+	}
+	public SolveInstruction getStep(int stepIndex)
+	{
+		//returns the SolveInstruction at the stepIndex
+	}
+	
+	
+}
